13b42769b3925b7c118398552693ff27cc70a20c
priyanshush325 <priyanshush325@gmail.com>
2025-03-30 18:16:10 -0700

    temporary sign out button

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import UserInfo from "@/app/components/user-info";
 import { useFirebaseAuth } from "@/app/hooks/firebase";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { SignoutButton } from "@/app/components/SignoutButton";
 
 export default function Home() {
   const auth = useFirebaseAuth();
@@ -18,5 +19,12 @@ export default function Home() {
   if (auth.isLoading) {
     return <div>Loading auth state...</div>;
   }
-  return auth.user ? <UserInfo user={auth.user} /> : <p>Not signed in...</p>;
+  return auth.user ? (
+    <div>
+      <UserInfo user={auth.user} />
+      <SignoutButton />
+    </div>
+  ) : (
+    <p>Not signed in...</p>
+  );
 }
