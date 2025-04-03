70fdf7d1889097972f4adf69aa65754a055f7972
priyanshush325 <priyanshush325@gmail.com>
2025-04-03 12:56:54 -0700

    fix: fix build issue by uncommenting edit shop

--- app/edit-shop/page.tsx
+++ app/edit-shop/page.tsx
@@ -1,23 +1,9 @@
-// "use client";
+"use client";
 
-// import { useState } from "react";
-// // import EditShopModal from "../components/EditShopModal";
-// import { Pencil } from "lucide-react";
-
-// export default function EditShopPage() {
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-
-//   return (
-//     <div className="mx-auto flex min-h-screen max-w-md flex-col bg-white">
-//       {/* Floating Edit Button */}
-//       <div className="fixed top-7 left-1/2 -translate-x-1/2">
-//         <button onClick={() => setIsModalOpen(true)} className="p-3">
-//           <Pencil className="size-6 text-gray" />
-//         </button>
-//       </div>
-
-//       {/* Modal (Controlled Externally) */}
-//       {/* <EditShopModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} /> */}
-//     </div>
-//   );
-// }
+export default function EditShopPage() {
+  return (
+    <div className="mx-auto flex min-h-screen max-w-md flex-col bg-white">
+      Placeholder
+    </div>
+  );
+}
