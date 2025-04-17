3d9702c596cad5b50b2828b1ef0e65d0d337abe7
srikarnimma <nsrikar18@gmail.com>
2025-04-17 09:21:43 -0700

    Added temp loading animation to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import UserInfo from "@/app/components/user-info";
 import { useFirebaseAuth } from "@/app/hooks/firebase";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import { SignoutButton } from "@/app/components/SignoutButton";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "@/app/lib/client/firebase";
 
-export default function Home() {
+export default function HomeRedirect() {
   const auth = useFirebaseAuth();
   const router = useRouter();
 
@@ -32,7 +30,7 @@ export default function Home() {
             const shopHandle = shopDoc.username;
             router.push(`/${shopHandle}`);
           } else {
-            console.warn("No shop found for this user.");
+            router.push("/onboarding"); // fallback if no shop exists
           }
         } catch (err) {
           console.error("Error fetching shop info:", err);
@@ -43,16 +41,10 @@ export default function Home() {
     checkUserAndRedirect();
   }, [auth, router]);
 
-  if (auth.isLoading) {
-    return <div>Loading auth state...</div>;
-  }
-
-  return auth.user ? (
-    <div>
-      <UserInfo user={auth.user} />
-      <SignoutButton />
+  // Loading animation
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="size-10 animate-spin rounded-full border-y-2 border-gray-900" />
     </div>
-  ) : (
-    <p>Not signed in...</p>
   );
 }
