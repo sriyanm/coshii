8f18db8ea08ee3ba23507d34204b758573ac7e28
Sriyan Madugula <sriyan@umich.edu>
2025-05-09 12:41:38 -0400

    listed button visibility issue fixed

--- app/components/ui/switch.tsx
+++ app/components/ui/switch.tsx
@@ -18,7 +18,7 @@ export const Switch = ({
       role="switch"
       aria-checked={checked}
       onClick={() => onCheckedChange(!checked)}
-      className={`relative inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 ${checked ? (onCheckedColor ? onCheckedColor : "bg-orange-500") : "bg-gray-200"} ${className}`}
+      className={`relative inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 ${checked ? (onCheckedColor ? onCheckedColor : "bg-orange") : "bg-gray-200"} ${className}`}
     >
       <div
         className={`pointer-events-none block size-5 rounded-full bg-white shadow-lg ring-0 transition-transform ${checked ? "translate-x-5" : "translate-x-0"}`}
