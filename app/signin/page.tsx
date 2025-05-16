8e45232169a1f33c58b4bdfbeae73db37dd1518b
Sriyan Madugula <sriyan@umich.edu>
2025-05-15 22:46:16 -0400

    signin with new account redirects to error page that redirects to onboarding

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -9,7 +9,7 @@ import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "@/app/lib/client/firebase";
 import { Alert, AlertDescription, AlertTitle } from "@/app/components/ui/alert";
 import { AlertCircle } from "lucide-react";
-import Link from "next/link";
+import { Button } from "@/app/components/ui/button";
 
 export default function SignInPage() {
   const { user, isLoading } = useFirebaseAuth();
@@ -46,44 +46,56 @@ export default function SignInPage() {
 
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-5 px-4">
-      <GoogleSigninButton 
-        allowNewUser={false}
-        onError={(error) => {
-          console.log("Google sign-in error:", error);
-          if (error instanceof Error && error.message === "New users are not allowed") {
-            setErrorMessage("It seems like this is your first time here. Please set up your shop.");
-            // router.push("/onboarding");
-          }
-          else if (error instanceof Error) {
-            setErrorMessage("Failed to sign in with Google. Please try again.");
-          }
-        }}
-      />
-      {errorMessage && (
+      {!errorMessage && (
+        <GoogleSigninButton 
+          allowNewUser={false}
+          onError={(error) => {
+            console.log("Google sign-in error:", error);
+            if (error instanceof Error && error.message === "New users are not allowed") {
+              setErrorMessage("It seems like this is your first time here. Please set up your shop.");
+              // router.push("/onboarding");
+            }
+            else if (error instanceof Error) {
+              setErrorMessage("Failed to sign in with Google. Please try again.");
+            }
+          }}
+        />
+      )}
+      {errorMessage && !errorMessage.endsWith("Please set up your shop.") && (
         <Alert
           variant={
-            errorMessage.endsWith("Please set up your shop.")
-              ? "default"
-              : "destructive"
+            "destructive"
           }
         >
           <AlertCircle className="size-4" />
           <AlertTitle>Error</AlertTitle>
           <AlertDescription>
-            {errorMessage.endsWith("Please set up your shop.") ? (
-              <>
-                It seems like this is your first time here.{" "}
-                {/* TODO: styling of this message */}
-                <Link href="/onboarding" className="text-blue-500 underline">
-                  Please set up your shop here
-                </Link>
-                .
-              </>
-            ) : (
-              errorMessage
-            )}
+            {errorMessage}
           </AlertDescription>
         </Alert>
+      )}
+      {errorMessage && errorMessage.endsWith("Please set up your shop.") && (
+        <div className="fixed inset-0 mx-auto flex min-h-screen max-w-md flex-col items-center p-8">
+          <div>
+            <h1 className="mb-6 mt-4 text-start text-xl font-bold text-gray-600">
+            You don&apos;t have a shop yet, let&apos;s get you set up now.
+            </h1>
+          </div>
+          <div className="fixed bottom-0 mx-auto w-full max-w-md px-10 py-4">
+            <div className="flex justify-center">
+              <Button
+                id="recaptcha-element"
+                className="w-full min-w-24 py-6 text-lg"
+                variant="onboarding"
+                onClick={function () {
+                  router.push("/onboarding");
+                }}
+              >
+                {"Continue"}
+              </Button>
+            </div>
+          </div>
+        </div>
       )}
       {/* <MagicLinkSigninForm /> */}
     </div>
