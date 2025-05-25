cf5a4220595960476050861b52c726bf39dfec4c
Sriyan Madugula <sriyan@umich.edu>
2025-05-25 16:00:57 -0400

    signing out... loading button display when signing out

--- app/components/SignoutButton.tsx
+++ app/components/SignoutButton.tsx
@@ -1,5 +1,6 @@
 import { useSignOut } from "@/app/hooks/firebase";
 import { Button } from "./ui/button";
+import { useState } from "react";
 
 type Props = {
   shopHandle: string;
@@ -7,14 +8,15 @@ type Props = {
 
 export function SignoutButton({ shopHandle }: Props) {
   const signOut = useSignOut(shopHandle);
-
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <Button
       onClick={() => {
         signOut.mutate();
+        setIsLoading(true);
       }}
     >
-      Sign out
+      {isLoading ? "Signing out..." : "Sign out"}
     </Button>
   );
 }
